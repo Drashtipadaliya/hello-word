e409b5210f0421af6aab35fbf6e53e57eb8564f2
Drashtipadaliya <126863226+Drashtipadaliya@users.noreply.github.com>
2024-12-29 02:54:10 -0500

    Delete todo-cli directory

--- todo-cli/__tests__/first.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-// __tests__/first.js
-
-describe("First Test Suite", () => {
-  test("should run a basic test", () => {
-    expect(true).toBe(true); // This is a simple assertion
-  });
-});
